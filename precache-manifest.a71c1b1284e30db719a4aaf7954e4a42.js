fe0b0af62cc1bc58cc54b03181325da3a3cace39
Mark Mbugua <mbuguamark@gmail.com>
2019-07-12 11:04:31 +0300

    Updates

---
+++ precache-manifest.a71c1b1284e30db719a4aaf7954e4a42.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1f1f73ded25561c19062fb9f8e1455",
+    "url": "/edamam-recipes-app/index.html"
+  },
+  {
+    "revision": "e348025767bcc529c8a8",
+    "url": "/edamam-recipes-app/static/css/main.55db47d0.chunk.css"
+  },
+  {
+    "revision": "d27ba6dadf7b6d6d23f2",
+    "url": "/edamam-recipes-app/static/js/2.aa9b191f.chunk.js"
+  },
+  {
+    "revision": "e348025767bcc529c8a8",
+    "url": "/edamam-recipes-app/static/js/main.910a0bbe.chunk.js"
+  },
+  {
+    "revision": "dfb49de3e7a15c8d3d75",
+    "url": "/edamam-recipes-app/static/js/runtime~main.255af5d1.js"
+  }
+]);
